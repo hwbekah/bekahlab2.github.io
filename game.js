25035695a1592db819e956d5bc347460698ff06c
Rebekah Tong <90772550+hwbekah@users.noreply.github.com>
2021-10-24 18:30:01 +0100

    Implemented calculation of longest escape duration

--- game.js
+++ game.js
@@ -211,6 +211,18 @@ function isHit(defender, offender) {
     let score = hits.innerHTML;
     score = Number(score) + 1; //increment the score
     hits.innerHTML = score; //display the new score
+
+    // calculate longest duration
+    let newStingTime = new Date();
+    let thisDuration = newStingTime - lastStingTime;
+    lastStingTime = newStingTime;
+    let longestDuration = Number(duration.innerHTML);
+    if (longestDuration === 0) {
+      longestDuration = thisDuration;
+    } else {
+      if (longestDuration < thisDuration) longestDuration = thisDuration;
+    }
+    document.getElementById("duration").innerHTML = longestDuration;
   }
 }
 
